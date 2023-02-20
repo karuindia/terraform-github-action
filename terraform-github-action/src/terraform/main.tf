042762575073552bb286cc2de02ac319051e1064
Karunakaran K <112243704+karu-arch@users.noreply.github.com>
2023-02-20 23:06:28 +0530

    workspace updated111122

--- terraform-github-action/src/terraform/main.tf
+++ terraform-github-action/src/terraform/main.tf
@@ -1,35 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-
- backend "azurerm" {
-    resource_group_name  = "saturday-demo-rg"
-    storage_account_name = "sttfstatemgt01"
-    container_name       = "tfstate"
-    key                  = "demo.terraform.tfstate"
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  # tenant_id       = var.tenant_id
+    features {}
 }
 
-
-# Create a resource group
+#create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "demo-rg"
-  location = "West Europe"
-  tags =  {
-    env = "dev"
-    cost = "TA102AXY"
-  }
-}
+    name = "examplerg1"
+    location = "Central India"
